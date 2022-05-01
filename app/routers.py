52293e767cfaca483b25a066dad53425417b3267
ivand1988 <ivand1988@outlook.com>
2022-05-01 16:50:29 +0300

    Refactor stocks put + added stocks router

--- app/routers.py
+++ app/routers.py
@@ -1,5 +1,6 @@
 from app.etf import router_etf
 from app.auth import router_auth
+from app.main import router_stocks
 from fastapi import FastAPI
 
 
@@ -10,8 +11,15 @@ app.include_router(
     prefix="/etf",
     tags=["etf"]
 )
+
 app.include_router(
     router_auth,
     prefix="/auth",
     tags=["auth"]
+)
+
+app.include_router(
+    router_stocks,
+    prefix="/stocks",
+    tags=["stocks"]
 )
